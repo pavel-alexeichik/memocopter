45082b3b315b560e31b3c5bd39fc44d0cca7d0d0
Pavel Alexeichik <pavel.alexeichik@gmail.com>
2016-09-06 09:42:51 +0300

    Dashboard welcome message + capybara

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'dashboard#index'
   resources :cards_sets
   resources :cards
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
