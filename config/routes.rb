ea8350f54afbec13b377fb139662c6811dde5b6c
Pavel Alexeichik <pavel.alexeichik@gmail.com>
2016-09-06 19:36:20 +0300

    sign up small fix

--- config/routes.rb
+++ config/routes.rb
@@ -9,15 +9,9 @@ Rails.application.routes.draw do
     root 'home#landing', as: :unauthenticated
   end
 
-  # devise_for :users
-  # devise_for :users, :skip => [:sessions]
+  devise_for :users, :skip => [:registrations]
   devise_scope :user do
-    post 'signup' => 'devise/registrations#create', :as => :user_registration
-    get 'signin' => 'devise/sessions#new', :as => :new_user_session
-    post 'signin' => 'devise/sessions#create', :as => :user_session
-    # delete "/logout" => "devise/sessions#destroy"
-    get 'users/confirmation/new' => 'devise/confirmations#new', :as => 'new_user_confirmation'
-    get 'users/unlock/new' => 'devise/unlocks#show', :as => 'new_user_unlock'
+    post 'signup', to: 'devise/registrations#create', as: :user_registration
   end
 
 end
