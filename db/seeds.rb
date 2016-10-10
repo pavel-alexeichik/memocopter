c72d590efb54ea7b7101d74ad19108809f37aaa0
Pavel Alexeichik <pavel.alexeichik@gmail.com>
2016-10-10 18:44:27 +0300

    db:seeds for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+if Rails.env.development?
+  Card.delete_all
+  User.delete_all
+  (1..9).each do |num|
+    user = User.create!(email: "test#{num}@test.com",
+      display_name: "test#{num}@test.com",
+      password: 'qweasd')
+    user.cards.delete_all
+    (1..20).each do |card_num|
+      card = Card.new(question: "question#{card_num}",
+        answer: "answer#{card_num}")
+      user.cards << card
+    end
+  end
+end
